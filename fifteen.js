e0798a8631dd0d27178165a536d343d3e166a8a5
Travis Smith <travissmith94@hotmail.com>
2014-10-30 07:51:51 -0500

    Fills in last part of picture upon puzzle completion

--- fifteen.js
+++ fifteen.js
@@ -1,4 +1,5 @@
 var piece;
+var image = "S";
 var emptyX = "300px";
 var emptyY = "300px";
 var done = false;
@@ -16,7 +17,16 @@ function isFinished() {
 function endGame(){
     for (var i = 0; i < piece.length; i++){
         piece[i].innerHTML = "";
+        piece[i].style.border = "2px solid black";
+        piece[i].style.color = "#000000";
     }
+    $("puzzlearea").innerHTML += '<div class = "puzzlepiece" id = "lastDiv"></div>'
+    var lastDiv = $("lastDiv")
+    lastDiv.style.top = "300px";
+    lastDiv.style.left = "300px";
+    lastDiv.style.backgroundImage = piece[0].style.backgroundImage;
+    lastDiv.style.backgroundPosition = "-300px -300px";
+    console.log($("puzzlearea"));
     done = true;
     alert("Puzzle Solved");
 }
@@ -83,7 +93,9 @@ window.onload = function () {
     $("controls").innerHTML += '<button class = "changePic">Spiderman</button>';
     $("controls").innerHTML += '<button class = "changePic">Gir</button>';
     $("controls").innerHTML += '<button class = "changePic">Power Puff Girls</button>';
+
     piece = $$("div#puzzlearea div");
+    
     selections = $$(".changePic");
     console.log(selections.length);
     
@@ -118,9 +130,35 @@ window.onload = function () {
    $("shufflebutton").onclick = function(){
         if(done){
             done = false;
+            $("lastDiv").remove();
+            piece = $$("div#puzzlearea div");
+            console.log(piece.length);
             for(var i= 1; i <= 15; i++){
                 piece[i-1].innerHTML = i;
+                piece[i-1].onclick = function(){
+                    if(canMove(parseInt(this.innerHTML))){
+                        swap(parseInt(this.innerHTML) - 1);
+                        if(isFinished()){
+                        //console.log("Game Done!");
+                            endGame();
+                        }
+                    }   
+                };
+                
+                piece[i-1].onmouseover = function(){
+                    if(canMove(parseInt(this.innerHTML))){
+                        this.style.border = "2px solid red";
+                        this.style.color = "#006600";
+                    }
+                };
+     
+                piece[i-1].onmouseout = function(){
+                    this.style.border = "2px solid black";
+                    this.style.color = "#000000";
+                };
             }
+           
+           
         }
        
         for (var i=0; i<1000; i++)
@@ -130,7 +168,7 @@ window.onload = function () {
             if(canMove(parseInt(piece[rand - 1].innerHTML))){
                 
                 swap(parseInt(piece[rand-1].innerHTML) - 1);
-                console.log(emptyX + " " + emptyY);
+               // console.log(emptyX + " " + emptyY);
             }
         }
 	};
@@ -139,16 +177,19 @@ window.onload = function () {
             if(this.innerHTML === "Spiderman"){
                for(var j = 0; j <piece.length; j++){
                     piece[j].style.backgroundImage = 'url("background.jpg")';
+                   image = "S";
                 }
             }
             else if(this.innerHTML === "Gir"){
                 for(var j = 0; j <piece.length; j++){
                     piece[j].style.backgroundImage = 'url("background2.png")';
+                    image = "G";
                 }
             }
              else if(this.innerHTML === "Power Puff Girls"){
                for(var j = 0; j <piece.length; j++){
                     piece[j].style.backgroundImage = 'url("background3.jpg")';
+                   image = "P";
                 }
             }
         }
